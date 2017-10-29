d1009d810d6707d23957d62051abe9296043fa40
Andrei Radzetski <andrei.radzetski@gmail.com>
2017-10-29 19:09:53 +0100

    Projects page

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const DIST_CSS = "./dist/css"
 const DIST_LIB = "./dist/lib"
 const SASS_SRC = "./src/sass/**/*.scss"
 const CSS_BUNDLE = "main.css"
-const PAGES = ["./src/index.html"]
+const PAGES = ["./src/index.html", "./src/projects.html"]
 const LIB = ["./src/lib/font-awesome-4.7.0/css/**/*",
   "./src/lib/font-awesome-4.7.0/fonts/**/*"]
 
